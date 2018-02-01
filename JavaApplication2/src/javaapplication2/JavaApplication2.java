4d490af8efa47135d7dab1a9c3f6c9968b1171a4
if17 <if17@A406-14.INFORMAATIKA>
2018-02-01 09:22:20 +0200

    Uue projekti loomine

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ * Esimene katsetus. Muutujad, valik, massiiv
+ * @author if17
+ */
+public class JavaApplication2 {
+
+    /**
+     * Programmi töö algus koht
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printIn("Hello, web");
+    }
+    
+}
