b5b457959ec9deb802274c212270bfaa42f62395
if17 <if17@A406-14.INFORMAATIKA>
2018-02-01 09:33:14 +0200

    Ilma kommentaar

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -11,12 +11,20 @@ package javaapplication2;
  */
 public class JavaApplication2 {
 
+    
+    public static void ilmaKommentaar(){
+        int temperatuur =-2;
+        if(temperatuur>0){System.out.println("Sulab");}
+        else{System.out.println("Jäätab");}
+    }
     /**
      * Programmi töö algus koht
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.printIn("Hello, web");
+        System.out.println("Hello, Web!");
+        ilmaKommentaar();
+    
     }
     
 }
